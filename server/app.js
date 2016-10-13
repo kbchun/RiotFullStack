385ae37e2d0e4fdcdb3179bee6891e1a59b40a13
Kyle Chun <kbchun5712@gmail.com>
2016-10-13 14:29:08 -0700

    Server handles client GET requests for nonexistent summoners

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ var riotEndpts = {
   summonerID: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/',
   gameLog: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/',
   apiKey: '?api_key=4a99a25b-dd7e-4698-8256-9b3b773fa3ed'
-}
+};
 
 //================================================================================
   // Middleware
@@ -25,23 +25,29 @@ var riotEndpts = {
 //================================================================================
 app.use(express.static( __dirname + '/../client'));
 
-app.get('/name', function(req, response) {
-  console.log('receive GET request')
-  var summoner = req.url.slice(req.url.indexOf('?') + 1);
+// handle get request from client
+app.get('/name', function(reqClient, resClient) {
+  console.log('receive GET request');
+  var summoner = reqClient.url.slice(reqClient.url.indexOf('?') + 1);
   var riotId;
   var recentGameData;
 
-  request.get(riotEndpts.summonerID + summoner + riotEndpts.apiKey, function(err, req, res) {
-    riotId = JSON.parse(res)[summoner]['id'];
-    if(JSON.parse(res)[summoner]) {
-      request.get(riotEndpts.gameLog + riotId + '/recent' + riotEndpts.apiKey, function(err, req, res) {
-        recentGameData = util.dataCleaner(JSON.parse(res).games);
-        response.writeHead(201);
-        response.end(JSON.stringify(recentGameData));
-      })
+  // send get request to riot
+  request.get(riotEndpts.summonerID + summoner + riotEndpts.apiKey, function(err, reqRiot, resRiot) {
+    if (JSON.parse(resRiot).hasOwnProperty(summoner)) {
+      riotId = JSON.parse(resRiot)[summoner]['id'];
+      if (JSON.parse(resRiot)[summoner]) {
+        request.get(riotEndpts.gameLog + riotId + '/recent' + riotEndpts.apiKey, function(err, reqRiot, resRiot) {
+          recentGameData = util.dataCleaner(JSON.parse(resRiot).games);
+          resClient.writeHead(201);
+          resClient.end(JSON.stringify(recentGameData));
+        });
+      }
+    } else {
+      resClient.send(404).end();
     }
   });
-})
+});
 
 console.log('Server is listening on port 1337');
 app.listen(1337);
